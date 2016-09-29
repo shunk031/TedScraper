254fc8a4bd546bd58ea2e53bb2592d1c9e40dcd5
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-09-29 17:19:08 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import ted_talks
 
 setup(
     name='ted-scraper',
-    version='',
+    version=ted_talks.__version__,
     description='Scraper for TED Talks in Python. Get talk title, transcript, topics and so on.',
     author='Shunsuke KITADA',
     url='https://github.com/shunk031/ted-scraper',
+    keywords='TED Talks, TED, scraper, scraping',
     install_requires=['beautifulsoup4', 'lxml'],
     license='http://www.apache.org/licenses/LICENSE-2.0',
     test_suite='test.test_scraper'
