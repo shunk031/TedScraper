42e4f668f7221f2db77ef0f31311924af0c093f3
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-09-30 16:48:58 +0900

    add connect script

---
+++ tests/connect.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# import the module
+from __future__ import print_function
+import aerospike
+import pandas as pd
+import json
+import os
+
+# Configure the client
+config = {
+    'hosts': [('10.29.36.182', 3000)]
+}
+
+# Create a client and connect it to the cluster
+try:
+    client = aerospike.client(config).connect()
+except:
+    import sys
+    print("failed to connect to the cluster with", config['hosts'])
+    sys.exit(1)
+
+# Records are addressable via a tuple of (namespace, set, key)
+key = ('test', 'foo', 'bar')
+
+dir_list = os.listdir(
+    "/home/cyberangel/Programing/Python/ted-scraper-dev/02/ted-scraper/dump_files")
+
+
+for dl in dir_list:
+    with open("/home/cyberangel/Programing/Python/ted-scraper-dev/02/ted-scraper/dump_files/" + dl, "r") as f:
+        data = json.load(f)
+        print("add %s" % dl)
+        try:
+            # Write a record
+            client.put(key, data)
+
+        except Exception as e:
+            import sys
+            print("error: {0}".format(e), file=sys.stderr)
+# Read a record
+(key, metadata, record) = client.get(key)
+print(key, metadata, record)
+
+# Close the connection to the Aerospike cluster
+client.close()
